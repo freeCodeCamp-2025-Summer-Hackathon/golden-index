a5b74202fa8bc680350f129814ac716ddb3cfcb3
VeroMestre <76122582+Chromotripsis@users.noreply.github.com>
2025-07-03 16:02:26 +0100

    Created the organisation model and added pivot table for users.

---
+++ app/Models/Organisation.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Notifications\Notifiable;
+use App\Traits\HasUuid;
+
+class Organisation extends Model
+{
+    use HasFactory, Notifiable, HasUuid;
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'email',
+        'phone',
+        'address',
+        'website',
+        'is_verified',
+        'is_active',
+        'contact_info',
+        'mission_statement',
+        'org_type',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [
+        
+    ];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'created_at' => 'datetime',
+            'updated_at' => 'datetime',
+        ];
+    }
+
+    /**
+     * The users that belong to the organisation.
+     */
+    public function users(): 
+    {
+        return $this->belongsToMany(User::class, 'users_organisations', 'organisation_id', 'user_id');
+    }
+
+}
