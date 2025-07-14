595219af7565de6a2a6e6663009cb186a027dde7
VeroMestre <76122582+Chromotripsis@users.noreply.github.com>
2025-07-14 17:45:28 +0100

    Changed reference to user table. it now refers to "id" instead of "user_id" in the users table

--- database/migrations/2025_07_07_084047_create_volunteer_time_log_table.php
+++ database/migrations/2025_07_07_084047_create_volunteer_time_log_table.php
@@ -25,7 +25,7 @@ return new class extends Migration
             $table->timestamp('created_at');
             $table->timestamp('updated_at')->nullable();
 
-            $table->foreign('user_id')->references('user_id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->foreign('event_id')->references('event_id')->on('events');
         });
     }
