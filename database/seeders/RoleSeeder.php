401e8c41a588afb598feb9e28488437f353c581a
Shirleen <shirleen@duck.com>
2025-07-27 14:15:56 +0100

    fixed the issue with getting all the events

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -4,36 +4,23 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
-use App\Models\User;
 
 class RoleSeeder extends Seeder
 {
     public function run(): void
     {
-        // Create roles first
-        Role::firstOrCreate(['name' => 'super-admin']);
-        Role::firstOrCreate(['name' => 'organisation-admin']);
-        Role::firstOrCreate(['name' => 'volunteer']);
-        Role::firstOrCreate(['name' => 'event-organiser']);
-        Role::firstOrCreate(['name' => 'user']);
-
-        // Assign roles to existing users
-        $roleAssignments = [
-            'shirleen@duck.com' => 'super-admin',
-            'vero.mestre11@gmail.com' => 'super-admin',
-            'shirleenjvr@gmail.com' => 'organisation-admin',
-            'shirleen@redpecil.dev' => 'volunteer',
-            'akanniwilliams@gmail.com' => 'volunteer',
+        // Just create the roles - assignment happens in DatabaseSeeder
+        $roles = [
+            'super-admin',
+            'organisation-admin', 
+            'volunteer',
+            'event-organiser',
+            'user'
         ];
 
-        foreach ($roleAssignments as $email => $roleName) {
-            $user = User::where('email', $email)->first();
-            if ($user) {
-                $user->assignRole($roleName);
-                $this->command->info("Role '{$roleName}' assigned to {$user->email}");
-            } else {
-                $this->command->warn("User with email {$email} not found");
-            }
+        foreach ($roles as $roleName) {
+            Role::firstOrCreate(['name' => $roleName]);
+            $this->command->info("Created role: {$roleName}");
         }
     }
 }
